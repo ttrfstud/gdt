e74394f0428bf095ce843aad20ccc835cc115910
ttrfstud <ttrfstud@gmail.com>
2014-05-06 03:57:04 +0300

    More test

--- fn/lenequiv.js
+++ fn/lenequiv.js
@@ -1,16 +1,35 @@
+var isdef = require('./isdef');
+
 function lenequiv(equiv) {
-  var hash;
-  var len, elen;
+  var ihash, jhash;
+  var ilen, jlen, elen, len;
   var i;
 
-  len = 0;
+  ilen = 0;
+  jlen = 0;
   elen = equiv.length;
 
+  ihash = {};
+  jhash = {};
+
   for (i = 0; i < elen; i++) {
-    if (!isdef(hash[equiv[i].i])) {
-      hash[equiv[i].i] = 1;
-      len++;
+    if (!isdef(ihash[equiv[i].i])) {
+      ihash[equiv[i].i] = 1;
+
+      ilen++;
     }
+
+    if (!isdef(jhash[equiv[i].j])) {
+      jhash[equiv[i].j] = 1;
+
+      jlen++;
+    }
+  }
+
+  if (jlen > ilen) {
+    len = ilen;
+  } else {
+    len = jlen;
   }
 
   return len;
